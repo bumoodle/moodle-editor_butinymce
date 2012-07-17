6573e360d4c964d389a9537723f87f3418f67fcf
Kyle Temkin <ktemkin@binghamton.edu>
2012-07-17 07:11:07 -0400

    EDITOR_BUTINYCME slight performance optimizations for tinyme

--- lib.php
+++ lib.php
@@ -134,7 +134,7 @@ class butinymce_texteditor extends texteditor {
                     'apply_source_formatting' => true,
                     'remove_script_host' => false,
                     'entity_encoding' => "raw",
-                    'plugins' => "{$xmedia}advimage,safari,table,style,layer,advhr,advlink,emotions,inlinepopups,searchreplace,paste,directionality,fullscreen,moodlenolink,{$xemoticon}{$xdragmath}nonbreaking,contextmenu,insertdatetime,save,iespell,preview,print,noneditable,visualchars,xhtmlxtras,template,pagebreak,spellchecker,pdw",
+                    'plugins' => "{$xmedia}advimage,safari,table,style,advhr,advlink,emotions,inlinepopups,searchreplace,paste,directionality,fullscreen,moodlenolink,{$xemoticon}{$xdragmath}nonbreaking,contextmenu,insertdatetime,save,iespell,noneditable,visualchars,xhtmlxtras,spellchecker,pdw",
                     'theme_advanced_font_sizes' => "1,2,3,4,5,6,7",
                     'theme_advanced_layout_manager' => "SimpleLayout",
                     'theme_advanced_toolbar_align' => "left",
